fcc1f0747a99deeda09dd187d4db5fe78e19b3a7
Daehun Kim <gidonskaris@gmail.com>
2020-07-26 15:44:46 +0900

    [Add] gravatar

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
+
+const userModel = require("../model/user");
 
 
 //회원가입기능
@@ -9,8 +13,70 @@ const router = express.Router();
 //@access PUBLIC
 router.post("/register", (req, res) => {
 
+    // 이메일 유무체크 -> 패스워드 암호화 -> 데이터베이스 저장
 
+    userModel
+        .findOne({email : req.body.email})
+        .then(email => {
+            if(email){
+                return res.json({
+                    message : "email already exists"
+                });
+            }
+            else{
 
+               const avatar = gravatar.url(req.body.email, {
+                    s : "200",
+                    r : "pg",
+                    d : "mm"
+                })
+
+                bcrypt.hash(req.body.password, 10, (err, hash) =>{
+
+                    if(err){
+                        return res.json({
+                            message : err.message
+                        })
+                    }
+                    else {
+                        const newUser = new userModel({
+                            name : req.body.name,
+                            email : req.body.email,
+                            password : hash,
+                            avatar : avatar
+                        });
+
+                        newUser
+                            .save()
+                            .then(user => {
+                                console.log(user);
+
+                                res.json({
+                                    id : user._id,
+                                    name : user.name,
+                                    email : user.email,
+                                    password : user.password,
+                                    avatar : user.avatar,
+                                    date : {
+                                        createdDate : user.createdAt,
+                                        updatedDate : user.updatedAt
+                                    }
+                                })
+                            })
+                            .catch(err => {
+                                res.json({
+                                    error : err.message
+                                })
+                            })
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                message : err.message
+            })
+        })
 })
 
 
