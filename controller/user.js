23ae233fc24fe46acdd28e111342679af7461bf7
Daehun Kim <gidonskaris@gmail.com>
2020-07-26 16:39:37 +0900

    [Update] controller added and refactoring

---
+++ controller/user.js
@@ -0,0 +1,104 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../model/user");
+
+
+function tokenGenerator (payload) {
+    return jwt.sign(
+        payload,
+        process.env.SECRETKEY,
+        {expiresIn: "1d"}
+    );
+};
+
+
+exports.register_user = (req, res) => {
+
+    // 이메일 유무체크 -> 패스워드 암호화 -> 데이터베이스 저장
+
+    const {name, email, password } = req.body;
+
+    userModel
+        .findOne({email})
+        .then(user => {
+            if(user){
+                return res.json({
+                    message : "email already exists"
+                });
+            }
+            else{
+
+
+                const newUser = new userModel({
+                    name, email, password
+                });
+
+                newUser
+                    .save()
+                    .then(user => {
+                        console.log(user);
+
+                        res.json({
+                            id : user._id,
+                            name : user.name,
+                            email : user.email,
+                            password : user.password,
+                            avatar : user.avatar,
+                            date : {
+                                createdDate : user.createdAt,
+                                updatedDate : user.updatedAt
+                            }
+                        })
+                    })
+                    .catch(err => {
+                        res.json({
+                            error : err.message
+                        })
+                    })
+            }
+        })
+        .catch(err => {
+            res.json({
+                message : err.message
+            })
+        })
+};
+
+exports.login_user = (req, res) => {
+
+    // 이메일 유무체크 -> 패스워드 비교 -> 로그인 -> 성공메세지(토큰발행)
+
+    const {email, password} = req.body;
+
+    userModel
+        .findOne({email})
+        .then(user => {
+            console.log(user);
+            if(!user){
+                return res.json({
+                    message : "email not exists"
+                })
+            }
+            else{
+                user.comparePassword(password, (err, isMatch) => {
+                    if(err || isMatch === false) {
+                        return res.json({
+                            message: "password Incorrect"
+                        })
+                    }
+                    else{
+                        const payload = { id : user._id, name : user.name, email : user.email, avatar : user.avatar}
+
+                        res.json({
+                            success : isMatch,
+                            tokenInfo : tokenGenerator(payload)
+                        })
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                error : err.message
+            })
+        })
+};
--- routes/user.js
+++ routes/user.js
@@ -1,77 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 
-const userModel = require("../model/user");
+const {
+    register_user,
+    login_user
+} = require("../controller/user");
 
-function tokenGenerator (payload) {
-    return jwt.sign(
-        payload,
-        process.env.SECRETKEY,
-        {expiresIn: "1d"}
-    );
-};
 
 //회원가입기능
 
 //@route POST http://localhost:5000/user/register
 //@desc Register user
 //@access PUBLIC
-router.post("/register", (req, res) => {
-
-    // 이메일 유무체크 -> 패스워드 암호화 -> 데이터베이스 저장
-
-    const {name, email, password } = req.body;
-
-    userModel
-        .findOne({email})
-        .then(user => {
-            if(user){
-                return res.json({
-                    message : "email already exists"
-                });
-            }
-            else{
-
-
-                const newUser = new userModel({
-                    name, email, password
-                });
-
-                newUser
-                    .save()
-                    .then(user => {
-                        console.log(user);
-
-                        res.json({
-                            id : user._id,
-                            name : user.name,
-                            email : user.email,
-                            password : user.password,
-                            avatar : user.avatar,
-                            date : {
-                                createdDate : user.createdAt,
-                                updatedDate : user.updatedAt
-                            }
-                        })
-                    })
-                    .catch(err => {
-                        res.json({
-                            error : err.message
-                        })
-                    })
-            }
-        })
-        .catch(err => {
-            res.json({
-                message : err.message
-            })
-        })
-})
-
-
-
-
+router.post("/register", register_user);
 
 
 
@@ -81,56 +22,7 @@ router.post("/register", (req, res) => {
 //@desc Login user
 //@access PUBLIC
 
-router.post("/login", (req, res) => {
-
-    // 이메일 유무체크 -> 패스워드 비교 -> 로그인 -> 성공메세지(토큰발행)
-
-    const {email, password} = req.body;
-
-    userModel
-        .findOne({email})
-        .then(user => {
-            console.log(user);
-            if(!user){
-                return res.json({
-                    message : "email not exists"
-                })
-            }
-            else{
-                user.comparePassword(password, (err, isMatch) => {
-                    if(err || isMatch === false) {
-                        return res.json({
-                            message: "password Incorrect"
-                        })
-                    }
-                    else{
-                        const payload = { id : user._id, name : user.name, email : user.email, avatar : user.avatar}
-
-                        res.json({
-                            success : isMatch,
-                            tokenInfo : tokenGenerator(payload)
-                        })
-                    }
-                })
-            }
-        })
-        .catch(err => {
-            res.json({
-                error : err.message
-            })
-        })
-
-
-
-    //jwt 토큰 생성
-
-
-
-
-})
-
-
-
+router.post("/login", login_user);
 
 
 
