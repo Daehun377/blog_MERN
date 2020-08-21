834f9a8553dfbefeff042e34d163d08071bc1d4a
DaehunKim <gidonskaris@gmail.com>
2020-08-22 07:11:55 +0900

    [Add] post register

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
+const checkAuth = passport.authenticate("jwt", {session : false});
+
+const postModel = require("../model/post");
+
+//@route  POST http://localhost:2000/post/register
+//@desc   Register post
+//@access Private
+
+router.post("/register", checkAuth, (req, res) => {
+
+    const newPost = new postModel({
+        text : req.body.text,
+        user : req.user.id,
+        name : req.user.name,
+        avatar : req.user.avatar
+    });
+
+    newPost
+        .save()
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message : err.message
+            })
+        })
+});
 
 
 
