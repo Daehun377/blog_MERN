ba0ba3a1c4dd9ac213658775fd387f315142df1c
DaehunKim <gidonskaris@gmail.com>
2020-08-22 07:39:40 +0900

    [Add] post get, detail get

--- routes/post.js
+++ routes/post.js
@@ -32,9 +32,96 @@ router.post("/register", checkAuth, (req, res) => {
 });
 
 
+//retrieve all post
+//@route  GET http://localhost:2000/post/
+//@desc   Retrieve all post
+//@access Public
+router.get("/", (req, res) => {
+
+    postModel
+        .find()
+        .sort({date : -1})
+        .then(posts => {
+            if(posts.length === 0){
+              return res.status(200).json({
+                  message : "게시물이 없습니다"
+              })
+            }
+            res.status(200).json({
+                count : posts.length,
+                postInfo : posts
+            })
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                message : err.message
+            })
+        })
+
+});
 
 
+//get detail post
+//@route  GET http://localhost:2000/post/:post_id
+//@desc   Retrieve detail post
+//@access Private
+router.get("/:post_id", checkAuth,(req, res) => {
 
+    const id = req.params.post_id;
+
+    postModel
+        .findById(id)
+        .then(post => {
+            return res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message : err.message
+            })
+        })
+});
+
+
+//update post
+//@route  Patch http://localhost:2000/post/:post_id
+//@desc   Update detail post
+//@access Private
+router.patch("/:post_id", checkAuth, (req, res) => {
+
+});
+
+
+//delete post
+//@route  Delete http://localhost:2000/post/:post_id
+//@desc   Delete detail post
+//@access Private
+router.delete("/:post_id", checkAuth, (req, res) => {
+
+    const id = req.params.post_id;
+
+    postModel
+        .findById(id)
+        .then(post => {
+            // 사용자 식별
+            if(post.user.toString() !== req.user.id){
+                return res.status(400).json({
+                    message : "user not authorized this post"
+                })
+            }
+            post
+                .remove()
+                .then(() => {
+                    res.status(200).json({success : true})
+                })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message : err.message
+            })
+        })
+
+});
 
 
 
